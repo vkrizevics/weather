d9cdde54e0f30ad198df4d2ba40e3041af2c80cb
Valerijs Krizevics <kvikaramba@gmail.com>
2025-07-21 22:17:06 +0300

    dev-1 Weather stations repository

---
+++ src/Repository/StationRepository.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\Station;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Station>
+ *
+ * @method Station|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Station|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Station[]    findAll()
+ * @method Station[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class StationRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Station::class);
+    }
+}
