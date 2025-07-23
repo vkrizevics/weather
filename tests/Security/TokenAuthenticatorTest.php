2b806f81a8e312738b93dc1681e90e17dccead0c
Valerijs Krizevics <kvikaramba@gmail.com>
2025-07-23 21:14:10 +0300

    dev-1 Working authenticator without deprecation notices

--- tests/Security/TokenAuthenticatorTest.php
+++ tests/Security/TokenAuthenticatorTest.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace App\Tests\Security;
 
-use App\Security\TokenGuardAuthenticator;
+use App\Security\TokenAuthenticator;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\Request;
 
-class TokenGuardAuthenticatorTest extends TestCase
+class TokenAuthenticatorTest extends TestCase
 {
     private const VALID_TOKEN = 'MY_SECRET_TOKEN';
 
@@ -17,7 +17,7 @@ class TokenGuardAuthenticatorTest extends TestCase
         $request = new Request();
         $request->headers->set('Authorization', 'Bearer ' . self::VALID_TOKEN);
 
-        $authenticator = new TokenGuardAuthenticator(self::VALID_TOKEN);
+        $authenticator = new TokenAuthenticator(self::VALID_TOKEN);
 
         $this->assertTrue($authenticator->supports($request));
     }
